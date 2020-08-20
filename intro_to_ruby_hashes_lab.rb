d76603bb37302014217594cbd7af819b8c6bc5bc
Serkan Sahin <ssahin55@outlook.com>
2020-08-20 21:43:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,9 +4,14 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  { :railroads => {:pieces => 4,
-    
-  } }
+  {:railroads => {
+    :pieces => 4
+    :rent_in_dollars => {} 
+    :names = {}
+  }
+  
+  
+  }
 
 end
 
